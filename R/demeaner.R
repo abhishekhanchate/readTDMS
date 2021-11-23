63eda84758d9cdcc209c62fdcefb586209fa9d71
Abhishek Hanchate <abhishek.hanchate@tamu.edu>
2021-11-22 23:51:30 -0600

    Style update and added initial roxygen skeleton

--- R/demeaner.R
+++ R/demeaner.R
@@ -1,12 +1,24 @@
+#' @title Data Stream Demeaner
+#' @description The function aims to take a data stream in a vector form and returns the
+#' demeaned data for further analysis. In short, the function calculates the mean of the
+#' data and element-wise substracts it from the entire vector
+#' @param datastream is a vector containing a one dimensional data stream
+#' @return The function returns a vector containing the demeaned data stream
+#' @export
+#' @import
+
 # Data Stream Demeaning Function - 18th Nov 2021 - Version 1
 # Demeaning Function
 demeaner <- function(datastream){
+
   # Checking for the Data Format
   dimm <- dim(datastream)
   if (len(dimm) != 1){
     stop(paste("The provided data stream cannot be demeaned since it is not a vector"))
   }
+
   # Demeaning the Data Stream
   demeaned_datastream <- datastream - mean(datastream)
+
   return(demeaned_datastream)
 }
--- R/spectro.R
+++ R/spectro.R
@@ -1,3 +1,24 @@
+#' @title Data Stream Spectrogram Generator
+#' @description The function aims to take a data stream in a vector form and based on the
+#' other input parameters specified, i.e. n, Fs, window, overlap, dbON, returns a list
+#' containing a vector of frequency components, time axis vector, frequency axis vector,
+#' and a plot of the spectrogram
+#' @param x is a vector containing a one dimensional data stream
+#' @param n is a integer (dyadic preferred) specifying the number of nfft points
+#' @param Fs is a integer specifying the sampling frequency
+#' @param window specifies the type of window to use for generating and plotting spectrogram
+#' @param overlap indicated the desired amount of overlap in the windowing process
+#' @param dbON is a Boolean indicating if we want to convert to decibels or NOT
+#' @return The function returns a list containing :
+#'   \itemize{
+#'   \item freq_comps - A vector of normalized frequency components associated with the data stream
+#'   \item timestamps - A vector of time stamps/points associated with the data stream spectrogram
+#'   \item frequencystamps - A vector of frequency stamps/points associated with the data stream spectrogram
+#'   \item spectro_img - A spectrogram plot with desired input parameters for the data stream
+#'}
+#' @export
+#' @import signal, oce, ggplot
+
 # Data Stream Spectrogram Function - 19th Nov 2021 - Version 1
 # Signal Processing Functions
 library(signal, warn.conflicts = F, quietly = T)
@@ -32,6 +53,10 @@ spectro <- function(x, n, Fs, window, overlap, dbON){
                xlab = 'Time [s]',
                drawPalette = T,
                decimate = F)
-
-  return (spec)
+  # Returns a list
+  # freq_comps - A vector of normalized frequency components
+  # timestamps - A vector of time stamps/points
+  # frequencystamps - A vector of frequency stamps/points
+  # spectro_img - A spectrogram plot with desired input parameters
+  return(list(freq_comps = P, timestamps = t, frequencystamps = f, spectro_img = img))
 }
