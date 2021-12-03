1a374420d40f54059a50aef1617c5e6fbdf9f763
Abhishek Hanchate <abhishek.hanchate@tamu.edu>
2021-12-03 00:46:24 -0600

    Updated the tdmsread.R Function

--- R/tdmsread.R
+++ R/tdmsread.R
@@ -1,13 +1,17 @@
 # Data Stream (.tdms Format) Reading Function - 2nd Dec 2021 - Version 1
-library(tdmsreader)
-f = file('tests/MC_Off_1_Acc.tdms', 'rb')
-main = TdmsFile$new(f)
-main$read_data(f, 0, 1)
-r = main$objects[[ "/'Untitled'/'Dev1/ai0'"]]
-t = r$time_track(start = 0, end = 1)
-s = r$data
-png('out.png')
-plot(t, s, xlab = 'Time', ylab = 'Volts')
-title('TDMS reader')
-dev.off()
-close(f)
+# Reading and Manipulating .tdms files Functions
+tdmsread <- function(file_input){
+  library(tdmsreader)
+  file_input = file('tests/file.tdms', 'rb')
+  main = TdmsFile$new(file_input)
+  main$read_data(file_input, 0, 1)
+  r = main$objects[[ "/'Untitled'/'Dev1/ai0'"]]
+  t = r$time_track(start = 0, end = 1)
+  s = r$data
+  dev.off()
+  close(f)
+  return(list(datastream = s, timestamps = t))
+}
+
+
+
