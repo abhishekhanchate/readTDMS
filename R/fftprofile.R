805869d81b9372793ee5a93448846f22718cda01
Abhishek Hanchate <abhishek.hanchate@tamu.edu>
2021-12-10 23:57:30 -0600

    Style update and added initial roxygen skeleton

--- R/fftprofile.R
+++ R/fftprofile.R
@@ -1,4 +1,15 @@
-
+# Top Frequency Component Finder Function
+#' @title Top Frequency Component Finder Function
+#' @description The function reads in a TDMS file and generates its Fast Fourier Transform (FFT). For
+#' the FFT calculation, we ignore the complex conjugates involved and only consider the Magnitudes of each
+#' complex number. After finding the frequency components, it returns a list of top frequencies based on
+#' specified n value
+#' @param data is an TDMS file Input
+#' @param frequencyPoints corresponds to the number of points for FFT
+#' @param n corresponds to the number of Top Frequencies to be returned
+#' @return a list of Top frequencies associated with the TDMS file data signal in the Frequency Domain
+#' @import tdmsreader
+#' @export
 fftprofile <- function(data, frequencyPoints = 10000, n){
   ts <- tdmsread(data)
   ts_data <- ts$datastream
