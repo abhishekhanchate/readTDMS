ba975b494f851a65b3dfd664b6c0ceba0112e5d3
Abhishek Hanchate <abhishek.hanchate@tamu.edu>
2021-12-11 14:39:00 -0600

    Got rid of unwanted import

--- R/spectro.R
+++ R/spectro.R
@@ -22,7 +22,7 @@
 #'   \item frequencystamps - A vector of frequency stamps/points associated with the data stream spectrogram
 #'   \item spectro_img - A spectrogram plot with desired input parameters for the data stream
 #'}
-#' @import signal, oce, ggplot
+#' @import signal, ggplot
 #' @export
 spectro <- function(x, n, Fs, window, overlap, dbON){
   # Checks on dbON: Can only be 1 or 0
