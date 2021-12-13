022e4775df7db5477b75d6448d25d34796e6237b
Abhishek Hanchate <abhishek.hanchate@tamu.edu>
2021-12-13 15:34:59 -0600

    Updated datafreq.R based on testing

--- R/datafreq.R
+++ R/datafreq.R
@@ -3,22 +3,41 @@
 #' @description The function reads in a TDMS file and generates its Fast Fourier Transform (FFT). For
 #' the FFT calculation, we ignore the complex conjugates involved and only consider the Magnitudes of each
 #' complex number.
-#' @param data is an TDMS file Input
+#' @param file_input is an TDMS file Input
 #' @param frequencyPoints corresponds to the number of points for FFT
 #' @return The function returns the plot of the TDMS file data signal in the Frequency Domain
 #' @import tdmsreader
 #' @export
-datafreq <- function(data, frequencyPoints = 10000){
-  ts <- tdmsread(data)
-  ts_data <- ts$datastream
+datafreq <- function(file_input, frequencyPoints = 10000){
+
+  # Use the imported tdmsreader library to read the TDMS file
+  main <- TdmsFile$new(file_input)
+
+  # Extracting the First Channel and First Module in the TDMS file
+  main$read_data(file_input, 0, 1)
+
+  # Taking all streams associated with above specified Module and Channel
+  r <- main$objects[[ "/'Untitled'/'Dev1/ai0'"]]
+
+  # Extracting the Time stamps
+  t <- r$time_track(start = 0, end = 1)
+
+  # Extracting the Data stream values
+  ts_data <- r$data
+
+  # FFT Calculation
   data_fft <- fft(ts_data)
+
   # Ignore the 2nd half, which are complex conjugates of the 1st half,
   # and calculate the Mod (magnitude of each complex number)
   amplitude <- Mod(data_fft[1:(length(data_fft)/2)])
+
   # Calculate the frequencies
   frequency <- seq(0, frequencyPoints, length.out=length(data_fft)/2)
+
   # Plot!
   img <- plot(amplitude ~ frequency, t="l")
+
   # Return the Plot of TS data in the Frequency Domain
   return(img)
 }
