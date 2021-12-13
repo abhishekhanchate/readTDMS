17938a52c089bbc4f011f595ed71ed80daa85f92
Abhishek Hanchate <abhishek.hanchate@tamu.edu>
2021-12-13 16:46:19 -0600

    Fixed an error associated with frequency vector calculation

--- R/fftprofile.R
+++ R/fftprofile.R
@@ -1,5 +1,4 @@
-# Top Frequency Component Finder Function
-#' @title Top Frequency Component Finder Function
+#' Top Frequency Component Finder Function
 #' @description The function reads in a TDMS file and generates its Fast Fourier Transform (FFT). For
 #' the FFT calculation, we ignore the complex conjugates involved and only consider the Magnitudes of each
 #' complex number. After finding the frequency components, it returns a list of top frequencies based on
@@ -10,13 +9,17 @@
 #' @return a list of Top frequencies associated with the TDMS file data signal in the Frequency Domain
 #' @import tdmsreader
 #' @export
+#' @examples
+#' # Load the installed library/package
+#' library(readTDMS)
+#'
 fftprofile <- function(data, frequencyPoints = 10000, n){
   ts <- tdmsread(data)
   ts_data <- ts$datastream
   data_fft <- fft(ts_data)
-  amplitude <- Mod(data_fft[1:(length(data_fft)/2)])
-  frequency <- seq(0, frequencyPoints, length.out=length(data_fft)/2)
-  sorted <- sort.int(amplitude, decreasing=TRUE, index.return=TRUE)
+  amplitude <- Mod(data_fft[1:(length(data_fft) / 2)])
+  frequency <- seq(0, frequencyPoints, length.out = (length(data_fft) / 2) - 1)
+  sorted <- sort.int(amplitude, decreasing = TRUE, index.return = TRUE)
   top <- sorted$ix[1:n] # indexes of the largest n components
   return (frequencies[top]) # convert indexes to frequencies
 }
